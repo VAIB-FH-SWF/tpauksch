1480013f96afb6f463c4235e7e3ab556f63f01c7
Tim Pauksch <pauksch.tim@fh-swf.de>
2015-10-24 21:03:20 +0200

    Aufgabe_3.1 angelegt.
    Kommentare und ein paar grundlegende Eintragungen geschaffen.

---
+++ Aufgabe_3.1/src/Aufgabe_3.1.cpp
@@ -0,0 +1,36 @@
+//=============================================================================
+//              Filename:   RandomFeld.cpp
+//
+//          Beschreibung:   Programmierung mit C++ 1,
+//                          Blatt 7 / Aufgabe 3.1,
+//                          Feld mit Zufallszahlen belegen.
+//
+//              Aufgabe:    3.1
+//
+//      Praktikumsgruppe:   [V-08:30] - Is-VAI-B1A
+//                Author:   Tim Pauksch
+//      Erstellungsdatum:   24.10.2015
+//    Letzte Anpassungen:   24.10.2015
+//
+//               Version:   1.0
+//              Compiler:   g++
+//=============================================================================
+
+#include <iostream>
+#include <cstdlib>
+//#include <cmath>
+//#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+   //=============================================================================
+   // Variablendeclaration
+   //=============================================================================
+
+   double a[100];       // Feld für die Aufnahme von Zufallszahlen
+
+
+   return 0;
+}
