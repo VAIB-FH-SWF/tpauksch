8e3e25a295dda663a85c5b7d87ab25bc9bed791d
Tim Pauksch <pauksch.tim@fh-swf.de>
2015-11-09 22:16:49 +0100

    Abschluss Aufgabe 4.3

--- Aufgabe_4.2/src/countWords.cpp
+++ Aufgabe_4.2/src/countWords.cpp
@@ -10,7 +10,7 @@
 //      Praktikumsgruppe:   [V-08:30] - Is-VAI-B1A
 //                Author:   Tim Pauksch
 //      Erstellungsdatum:   08.11.2015
-//    Letzte Anpassungen:   08.11.2015
+//    Letzte Anpassungen:   09.11.2015
 //
 //               Version:   1.0
 //              Compiler:   g++
@@ -27,40 +27,40 @@ int main() {
    // Variablendeclaration
    //====================================================================================
 
-   int maxstring  = 256;    //Puffer bzw. Feldgroeße
-   int i          = 0;      //Schleifenzaehler und Feldindex
-   int summeAlpha = 0;      //Summe der Buchstaben
-   int summeDigit = 0;      //Summe der Zahlen
-   int summeWorte = 0;      //Summe der Worte
-   bool zustand = false;    //Zustandsvaiable (Wort: true/false)
-   char text[maxstring];    //Feld fuer die Aufnahme der Buchstaben/Ziffern
-   char c;                  //Zwischenspeicher fuer Buchstabe/Zahl
+   int  maxstring        = 256;    //Puffer bzw. Feldgröße
+   int  i                = 0;      //Schleifenzähler und Feldindex
+   int  summeAlpha       = 0;      //Summe der Buchstaben
+   int  summeDigit       = 0;      //Summe der Ziffern
+   int  summeWorte       = 0;      //Summe der Worte
+   bool zustand = false;           //Zustandsvaiable (Wort: true/false)
+   char text[maxstring];           //Feld für die Aufnahme der Buchstaben/Ziffern
+   char c;                         //Zwischenspeicher für Buchstabe/Ziffer
 
    //====================================================================================
    // Hauptprogramm
    //====================================================================================
 
-   cout << "***** Buchstaben und Zahlen abzählen *****\n" << endl;
-   cout << "Geben Sie eine Textzeile mit Zahlen ein: ";
+   cout << "***** Wörter in einem Text abzählen *****\n" << endl;
+   cout << "Geben Sie eine Textzeile ein: ";
 
    while ((c = cin.get()) != '\n') {                //Einzelzeichen lesen
       text[i] = c;                                  //und im Feld ablegen
-      i++;                                          //Schleifenzaehler erhoehen
+      i++;                                          //Schleifenzaehler erhöhen
    }
    text[i] = '\0';                                  //Zeichenkette abschließen
 
    for (i = 0; text[i] != '\0'; i++) {
-      if (isalpha(text[i])) {                        //Pruefung auf Buchstabe
+      if (isalpha(text[i])) {                       //Prüfung auf Buchstabe
          summeAlpha++;
 
-         //Pruefung ob sich der Buchstabe in einem Wort befindet
+         //Prüfung ob sich der Buchstabe in einem Wort befindet
          if (zustand == false && isalpha(text[i])) {
             summeWorte++;
          }
 
          zustand = true;                            //Zustand setzen
 
-      } else if (isdigit(text[i])) {                //Pruefung auf Zahl
+      } else if (isdigit(text[i])) {                //Prüfung auf Ziffer
          summeDigit++;
       } else {
          zustand = false;                           //Zustand 'false' bei Leerzeichen
@@ -73,7 +73,7 @@ int main() {
    cout << "Der eingegebene Text lautet: '" << text << "'" << endl;
    cout << setw(6) << summeAlpha << " Buchstaben" << endl;
    cout << setw(6) << summeWorte << " Worte" << endl;
-   cout << setw(6) << summeDigit << " Zahlen" << endl;
+   cout << setw(6) << summeDigit << " Ziffern" << endl;
    cout << "Programm beendet." << endl;
 
    return 0;
